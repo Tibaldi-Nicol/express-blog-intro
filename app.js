36e17ab91764dfc9e640d2f5ee8c40bf51b79c50
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-04-20 20:40:20 +0200

    [UPD]ADD-mio-blog

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("hello")
+//importo express
+const express = require('express');
+
+//inizializzo express in app cerando la variabile
+const app = express();
+
+//definisco la porta 
+const port=3000;
+//definisco la rotta di base 
+app.get('/',(req, res)=>{
+    //console.log("ciao");
+    res.send("server del mio blog");
+})
+
+//il server qui rimane ad ascoltare
+app.listen(port,()=>{
+    console.log("server in ascolto nella porta...")
+})
